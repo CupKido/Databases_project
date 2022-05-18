e5d0330ecf3e07c4974c5f31d78ac34186ea38a8
CupKido <saartaler@g.jct.ac.il>
2022-05-18 15:22:39 +0300

    added sql support config files and etc

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express')
+var bodyParser = require("body-parser");
+var mysql = require('mysql')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "html");
+
+app.engine("html", require("ejs").renderFile);
+
+app.get("/", function (req, res) {
+  /*   var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'saartaler',
+        password: 'mypassword',
+        server: 'localhost', 
+        database: 'SchoolDB' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from Student', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+app.post("/", (req, res) => {
+    res.send("Hello World");
+});*/
+  res.render("mainpage.html");
+});
+
+var con = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: 'saartaler',
+database: 'army'
+})
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var querya = con.query("select s.SoldierNum, s.Name, count(o.SoldierNum) as 'Op Count' from soldier s, soldier_in_op o where s.SoldierNum = o.SoldierNum group by s.SoldierNum, s.Name order by SoldierNum;", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+      
+      con.query("select distinct op.Location, op.Date, how_many_soldiers_in_op(op.Date, op.Location) from soldier_in_op op;", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+  });
+
+
+
