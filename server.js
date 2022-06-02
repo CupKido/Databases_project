7db913ab2049a5421b331336a13a9948d2760a5c
CupKido <saartaler@g.jct.ac.il>
2022-06-02 03:14:36 +0300

    better html, and more queries

--- server.js
+++ server.js
@@ -18,104 +18,144 @@ var con = mysql.createConnection({
   database: 'army'
 })
 
-app.get("/", function (req, res) {
-    console.log("Connected!");
-  /*   var sql = require("mssql");
-
-    // config for your database
-    var config = {
-        user: 'saartaler',
-        password: 'mypassword',
-        server: 'localhost', 
-        database: 'SchoolDB' 
-    };
-
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from Student', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
-});
-app.post("/", (req, res) => {
-    res.send("Hello World");
-});*/
-    res.render("mainpage.html")
-    //res.sendFile(__dirname + "/views/mainpage.html");
-  //res.render("mainpage.html");
-});
-
 var server = app.listen(80, function () {
-    console.log("Server is running on port " + port);
-  });
-
-app.post("/fill", function (req, res) {
-    console.log("stuff received")
-    const keys = Object.keys(req.body);
-    for (key of keys) {      
-        console.log(key);
-    }
-    people = "";
-    if( con.state === "disconnected" ){
-      con.connect(function(err) {
-        if (err) throw err;
-      });
-    }
-      console.log("Connected!");
-      var querya = con.query("select * from soldier s where s.SoldierNum =" + req.body["ID"] , function (err, result, fields) {
-          if (err) throw err;
-          a = [];
-          for(f in fields) {
-            a.push(fields[f]["name"]);
-          }
-          
-          console.log(a);
-          return res.render("secondpage.html",{result: result, fields: a, tryy: fields});
-          for (resa in result) {
-            console.log(result[resa]["Name"]);
-            people = people + result[resa]["Name"] + "\n";
-          }
-          return res.render("secondpage.html", {result: people, name: "helooooooo"});
-        });
-        
-    
+  console.log("Server is running on port " + port);
 });
 
-app.post("/giveq", function(req, res) {
-  if( con.state === "disconnected" ){
-    con.connect(function(err) {
+app.get("/", function (req, res) {
+  console.log("Connected!");
+
+  res.render("mainpage.html")
+});
+
+app.get("/giveq", function (req, res) {
+  res.render("404page.html");
+});
+
+app.get("/CustomQuery/:id" , function (req, res) {
+  var id = req.params.id;
+  console.log(id)
+  title = ""
+  current_query = ""
+  switch (id) {
+    case "Soldiers":
+      title = "All soldiers";
+      current_query = "select * from soldier s ORDER BY s.SoldierNum";
+      break;
+    case "Vehicles":
+      title = "All vehicles";
+      current_query = "select * from vehicle";
+      break;
+    case "Bases":
+      title = "All military bases";
+      current_query = "select * from military_base";
+      break;
+    case "Operations":
+      current_query = "select * from soldier_in_op";
+      break;
+    case "Weapons":
+      current_query = "select * from weapon"
+      break;
+    case "GazaLebanon":
+      title = "Soldier participated in Gaza or Lebanon operations";
+      current_query = "select * \
+      from soldier s, (select op.SoldierNum, op.Location \
+                      from soldier_in_op op \
+                      where op.Location = 'Gaza'\
+                      UNION\
+                      select op.SoldierNum, op.Location\
+                      from soldier_in_op op\
+                      where op.Location = 'Lebanon') sNum\
+      where s.SoldierNum = sNum.SoldierNum\
+      ORDER BY sNum.Location"
+      break;
+    case "OpCount":
+      title = "Operation counting for soldiers"
+      current_query = 
+      "select s.SoldierNum, s.Name, count(o.SoldierNum) as 'Op Count'\
+      from soldier s, soldier_in_op o\
+      where s.SoldierNum = o.SoldierNum\
+      group by s.SoldierNum, s.Name\
+      order by SoldierNum"
+      break;
+    default:
+        return res.render("404page.html");
+  }
+
+  if (con.state === "disconnected") {
+    con.connect(function (err) {
       if (err) throw err;
     });
   }
   console.log("Connected!");
-      var querya = con.query(req.body["query"] , function (err, result, fields) {
-          if (err) throw err;
-          a = [];
-          for(f in fields) {
-            a.push(fields[f]["name"]);
-          }
-          
-          console.log(a);
-          return res.render("secondpage.html",{result: result, fields: a});
-        });
+  var querya = con.query(current_query, function (err, result, fields) {
+    if (err) res.render("404page.html");
+    a = [];
+
+    for (f in fields) {
+      a.push(fields[f]["name"]);
+    }
+
+    console.log(a);
+    return res.render("secondpage.html", { result: result, fields: a, title: title });
+  });
+});
+
+
+
+app.post("/fill", function (req, res) {
+  console.log("stuff received")
+  const keys = Object.keys(req.body);
+  for (key of keys) {
+    console.log(key);
+  }
+  people = "";
+  if (con.state === "disconnected") {
+    con.connect(function (err) {
+      if (err) throw err;
+    });
+  }
+  console.log("Connected!");
+  var querya = con.query("select * from soldier s \
+  where s.SoldierNum =" + req.body["ID"], function (err, result, fields) {
+    if (err) throw err;
+    a = [];
+    for (f in fields) {
+      a.push(fields[f]["name"]);
+    }
+
+    console.log(a);
+    return res.render("secondpage.html", { result: result, fields: a, title: ("Soldier Number " + req.body["ID"] + ":") });
+  });
+
+
+});
+
+app.post("/giveq", function (req, res) {
+  if (con.state === "disconnected") {
+    con.connect(function (err) {
+      if (err) throw err;
+    });
+  }
+  console.log("Connected!");
+  var querya = con.query(req.body["query"], function (err, result, fields) {
+    if (err) res.render("404page.html");
+    a = [];
+    for (f in fields) {
+      a.push(fields[f]["name"]);
+    }
+
+    console.log(a);
+    return res.render("secondpage.html", { result: result, fields: a, title: "Custom query:" });
+  });
 
 });
 
 app.post("/goback", function (req, res) {
   res.render("mainpage.html")
 })
+
+
 /*
 var con = mysql.createConnection({
 host: 'localhost',
